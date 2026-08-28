ad21096959e145178c08138fb4b14e9c490554b4
SPPG-PARANG-TAMBUNG <yoloipin@gmail.com>
2026-08-28 08:54:26 +0800

    Update content.js

--- content.js
+++ content.js
@@ -20,18 +20,18 @@ window.LABEL_DATA = {
 
   serving: {
     // Format machineDate harus TAHUN-BULAN-TANGGAL.
-    machineDate: "2026-08-27",
-    date: "Kamis, 27 Agustus 2026",
+    machineDate: "2026-08-28",
+    date: "Jumat, 28 Agustus 2026",
   },
 
   // Cukup ganti bagian value untuk memperbarui daftar menu.
   // Jangan menekan Enter sebelum tanda kutip penutup.
   menu: [
     { value: "Nasi Putih" },
-    { value: "Chicken Katsu" },
-    { value: "Tahu Goreng Ketumbar" },
-    { value: "Vegetable Curry" },
-    { value: "Buah Semangka" },
+    { value: "Telur Bulat Saos Tiram" },
+    { value: "Perkedel Tahu" },
+    { value: "Tumis Buncis + Sawi Putih" },
+    { value: "Buah Apel" },
   ],
 
   // Ganti nilai setiap kelompok sesuai perhitungan petugas/ahli gizi.
@@ -40,41 +40,41 @@ window.LABEL_DATA = {
       {
         name: "Porsi Kecil",
         items: [
-          { label: "Energi", value: "588,65", unit: "kkal", highlight: true },
-          { label: "Protein", value: "26,00", unit: "g" },
-          { label: "Karbohidrat", value: "74,22", unit: "g" },
-          { label: "Lemak", value: "21,99", unit: "g" },
-          { label: "Serat", value: "0,97", unit: "g" },
+          { label: "Energi", value: "515,56", unit: "kkal", highlight: true },
+          { label: "Protein", value: "16,84", unit: "g" },
+          { label: "Karbohidrat", value: "70,43", unit: "g" },
+          { label: "Lemak", value: "19,95", unit: "g" },
+          { label: "Serat", value: "3,37", unit: "g" },
         ],
       },
       {
         name: "Porsi Besar",
         items: [
-          { label: "Energi", value: "643,02", unit: "kkal", highlight: true },
-          { label: "Protein", value: "28,53", unit: "g" },
-          { label: "Karbohidrat", value: "83,85", unit: "g" },
-          { label: "Lemak", value: "22,59", unit: "g" },
-          { label: "Serat", value: "1,00", unit: "g" },
+          { label: "Energi", value: "565,99", unit: "kkal", highlight: true },
+          { label: "Protein", value: "17,92", unit: "g" },
+          { label: "Karbohidrat", value: "81,55", unit: "g" },
+          { label: "Lemak", value: "20,21", unit: "g" },
+          { label: "Serat", value: "3,65", unit: "g" },
         ],
       },
       {
         name: "Porsi Balita",
         items: [
-          { label: "Energi", value: "544,02", unit: "kkal", highlight: true },
-          { label: "Protein", value: "24,95", unit: "g" },
-          { label: "Karbohidrat", value: "64,58", unit: "g" },
-          { label: "Lemak", value: "21,78", unit: "g" },
-          { label: "Serat", value: "0,95", unit: "g" },
+          { label: "Energi", value: "470,94", unit: "kkal", highlight: true },
+          { label: "Protein", value: "15,79", unit: "g" },
+          { label: "Karbohidrat", value: "60,79", unit: "g" },
+          { label: "Lemak", value: "19,74", unit: "g" },
+          { label: "Serat", value: "3,34", unit: "g" },
         ],
       },
       {
         name: "Porsi Bumil & Busui",
         items: [
-          { label: "Energi", value: "751,07", unit: "kkal", highlight: true },
-          { label: "Protein", value: "32,86", unit: "g" },
-          { label: "Karbohidrat", value: "103,98", unit: "g" },
-          { label: "Lemak", value: "23,98", unit: "g" },
-          { label: "Serat", value: "1,10", unit: "g" },
+          { label: "Energi", value: "690,84", unit: "kkal", highlight: true },
+          { label: "Protein", value: "23,35", unit: "g" },
+          { label: "Karbohidrat", value: "104,05", unit: "g" },
+          { label: "Lemak", value: "22,12", unit: "g" },
+          { label: "Serat", value: "4,25", unit: "g" },
         ],
       },
     ],
@@ -92,6 +92,6 @@ window.LABEL_DATA = {
     address: "Parang Tambung, Kota Makassar, Sulawesi Selatan",
     instagramLabel: "@sppg.parangtambung",
     instagramUrl: "https://www.instagram.com/sppgparangtambung",
-    lastUpdated: "27 Agustus 2026, 03.00 WITA",
+    lastUpdated: "28 Agustus 2026, 07.00 WITA",
   },
 };
