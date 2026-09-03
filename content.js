f06857e9aeadb869884bfa7eebdb05152ae06f5f
SPPG-PARANG-TAMBUNG <yoloipin@gmail.com>
2026-09-03 09:20:18 +0800

    Update content.js

--- content.js
+++ content.js
@@ -20,18 +20,18 @@ window.LABEL_DATA = {
 
   serving: {
     // Format machineDate harus TAHUN-BULAN-TANGGAL.
-    machineDate: "2026-09-02",
-    date: "Senin, 02 September 2026",
+    machineDate: "2026-09-03",
+    date: "Senin, 03 September 2026",
   },
 
   // Cukup ganti bagian value untuk memperbarui daftar menu.
   // Jangan menekan Enter sebelum tanda kutip penutup.
   menu: [
-    { value: "Nasi Putih" },
-    { value: "Ayam Kentucky" },
-    { value: "Tahu Sambal Tomat" },
-    { value: "Tumis Wortel + Sawi Hijau" },
-    { value: "Buah Jeruk Siam Madu" },
+    { value: "Lontong" },
+    { value: "Telur Bulat Kecap" },
+    { value: "Tahu Garlic" },
+    { value: "Sayur Steam Bumbu Pecel" },
+    { value: "Buah Kelengkeng" },
   ],
 
   // Ganti nilai setiap kelompok sesuai perhitungan petugas/ahli gizi.
@@ -40,41 +40,41 @@ window.LABEL_DATA = {
       {
         name: "Porsi Kecil",
         items: [
-          { label: "Energi", value: "604,36", unit: "kkal", highlight: true },
-          { label: "Protein", value: "18,47", unit: "g" },
-          { label: "Karbohidrat", value: "73,56", unit: "g" },
-          { label: "Lemak", value: "27,96", unit: "g" },
-          { label: "Serat", value: "4,01", unit: "g" },
+          { label: "Energi", value: "517,93", unit: "kkal", highlight: true },
+          { label: "Protein", value: "19,52", unit: "g" },
+          { label: "Karbohidrat", value: "65,43", unit: "g" },
+          { label: "Lemak", value: "21,05", unit: "g" },
+          { label: "Serat", value: "1,95", unit: "g" },
         ],
       },
       {
         name: "Porsi Besar",
         items: [
-          { label: "Energi", value: "691,38", unit: "kkal", highlight: true },
-          { label: "Protein", value: "21,60", unit: "g" },
-          { label: "Karbohidrat", value: "86,29", unit: "g" },
-          { label: "Lemak", value: "30,73", unit: "g" },
-          { label: "Serat", value: "4,93", unit: "g" },
+          { label: "Energi", value: "574,56", unit: "kkal", highlight: true },
+          { label: "Protein", value: "20,83", unit: "g" },
+          { label: "Karbohidrat", value: "78,09", unit: "g" },
+          { label: "Lemak", value: "21,28", unit: "g" },
+          { label: "Serat", value: "2,19", unit: "g" },
         ],
       },
       {
         name: "Porsi Balita",
         items: [
-          { label: "Energi", value: "559,73", unit: "kkal", highlight: true },
-          { label: "Protein", value: "17,42", unit: "g" },
-          { label: "Karbohidrat", value: "63,92", unit: "g" },
-          { label: "Lemak", value: "27,75", unit: "g" },
-          { label: "Serat", value: "3,98", unit: "g" },
+          { label: "Energi", value: "473,31", unit: "kkal", highlight: true },
+          { label: "Protein", value: "18,47", unit: "g" },
+          { label: "Karbohidrat", value: "55,79", unit: "g" },
+          { label: "Lemak", value: "20,83", unit: "g" },
+          { label: "Serat", value: "1,92", unit: "g" },
         ],
       },
       {
         name: "Porsi Bumil & Busui",
         items: [
-          { label: "Energi", value: "819,53", unit: "kkal", highlight: true },
-          { label: "Protein", value: "27,88", unit: "g" },
-          { label: "Karbohidrat", value: "105,81", unit: "g" },
-          { label: "Lemak", value: "33,82", unit: "g" },
-          { label: "Serat", value: "5,01", unit: "g" },
+          { label: "Energi", value: "778,51", unit: "kkal", highlight: true },
+          { label: "Protein", value: "33,15", unit: "g" },
+          { label: "Karbohidrat", value: "29,06", unit: "g" },
+          { label: "Lemak", value: "99,51", unit: "g" },
+          { label: "Serat", value: "2,38", unit: "g" },
         ],
       },
     ],
@@ -92,6 +92,6 @@ window.LABEL_DATA = {
     address: "Parang Tambung, Kota Makassar, Sulawesi Selatan",
     instagramLabel: "@sppg.parangtambung",
     instagramUrl: "https://www.instagram.com/sppgparangtambung",
-    lastUpdated: "02 September 2026, 03.00 WITA",
+    lastUpdated: "03 September 2026, 05.00 WITA",
   },
 };
